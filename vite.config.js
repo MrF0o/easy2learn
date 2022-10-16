ff4ab7d159aad04a05ecc5c088a3a9618c74a6e3
MrF0o <fathihelmi36@gmail.com>
2022-10-16 13:21:35 +0100

    add main.scss to vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 // 'resources/sass/oneui/themes/flat.scss',
                 // 'resources/sass/oneui/themes/modern.scss',
                 // 'resources/sass/oneui/themes/smooth.scss',
+                'resources/sass/main.scss',
                 'resources/js/oneui/app.js',
                 'resources/js/app.js',
                 'resources/js/pages/datatables.js',
