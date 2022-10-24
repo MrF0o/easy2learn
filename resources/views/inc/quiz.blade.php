98c0b23640e7fee4c737783f8ba9d47e494fac96
MrF0o <fathihelmi36@gmail.com>
2022-10-24 01:36:32 +0100

    fix footer alignment

--- resources/views/inc/quiz.blade.php
+++ resources/views/inc/quiz.blade.php
@@ -1,11 +1,26 @@
-<div class="card bg-lightgray w-100">
-    <div class="row card-body d-flex align-items-center justify-content-center text-md-left">
+<div class="card bg-lightgray overflow-hidden">
+
+    <div class="row justify-content-center align-items-center card-body">
+        <div class="col-lg-7 col-xl-6 col-md-10 d-flex justify-content-between">
+            <div class="text-md-left">
+                <h3 class="card-title">{{$title}}</h3>
+                <p>{{$quote}}</p>
+            </div>
+            <div class="col-md-5 d-flex align-items-center justify-content-end">
+                <div class=>
+                    <a href="/register" class="btn btn-primary btn-sm px-1 px-md-3">Try it out!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="card-body d-flex align-items-center justify-content-center">
         <div class="col-12 col-md-5 text-md-left">
             <h3 class="card-title">{{$title}}</h3>
             <p>{{$quote}}</p>
         </div>
-        <div class="col-12  col-md-3">
+        <div class="col-12  col-md-5">
             <a href="/register" class="btn btn-primary btn-sm px-1 px-md-3">Try it out!</a>
         </div>
-    </div>
+    </div> --}}
 </div>
