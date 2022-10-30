a55633851826b03b703ac3b7a33b399a0ab0cd50
MrF0o <fathihelmi36@gmail.com>
2022-10-30 08:45:21 +0100

    add google adsense

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,6 +36,15 @@
         gtag('config', 'UA-246383856-1');
     </script>
 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163553030411967"
+        crossorigin="anonymous"></script>
+    <!-- E2L horizontaal -->
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6163553030411967" data-ad-slot="2200440395"
+        data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
 </head>
 
 <body>
