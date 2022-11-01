e92d3b2989935ea9be8207717a78b5114e097ca7
MrF0o <fathihelmi36@gmail.com>
2022-11-01 17:43:05 +0100

    change ad place to fix the top spacing

--- resources/views/inc/product.blade.php
+++ resources/views/inc/product.blade.php
@@ -1,3 +1,12 @@
 <div class="bg-transparent" id="product">
-    
+
 </div>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163553030411967"
+    crossorigin="anonymous"></script>
+<!-- E2L horizontaal -->
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6163553030411967" data-ad-slot="2200440395"
+    data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
--- resources/views/pages/benefits.blade.php
+++ resources/views/pages/benefits.blade.php
@@ -12,19 +12,36 @@
                 <div class="card">
                     <div class="card-body">
                         <ul class="list-unstyled">
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you learn your stuff <span class="marked">30% FASTER</span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span class="marked">summarize</span> your course easily</li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you <span class="marked">don't need an interrogator</span>, the system interrogates you</li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span class="marked">easily search for keywords</span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can save all your courses in a <span class="marked">central place</span></span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span class="marked">export</span> your summary on PDF or Word</span></span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span class="marked">share your summary</span> with other students</span></span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span>earn money</span> by selling your summary in private or public</span></span></li>
-                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you <span class="marked">save the planet</span> (no waste of paper)</span></span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you learn your
+                                stuff <span class="marked">30% FASTER</span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span
+                                    class="marked">summarize</span> your course easily</li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you <span
+                                    class="marked">don't need an interrogator</span>, the system interrogates you</li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span
+                                    class="marked">easily search for keywords</span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can save
+                                all your courses in a <span class="marked">central place</span></span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span
+                                    class="marked">export</span> your summary on PDF or Word</span></span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can <span
+                                    class="marked">share your summary</span> with other students</span></span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you can
+                                <span>earn money</span> by selling your summary in private or public</span></span></li>
+                            <li class="fw-bold p-1"><i class="fa-solid fa-check me-1 text-primary fs-lg"></i>you <span
+                                    class="marked">save the planet</span> (no waste of paper)</span></span></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6163553030411967"
+            crossorigin="anonymous"></script>
+        <!-- E2L horizontaal -->
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6163553030411967" data-ad-slot="2200440395"
+            data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
     </div>
 @endsection
